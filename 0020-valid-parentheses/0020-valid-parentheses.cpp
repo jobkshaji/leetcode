59548083f18c6f48eb0d1c26e8b81936187b30cb
JOB K SHAJI <142813709+jobkshaji@users.noreply.github.com>
2026-03-06 10:51:00 +0530

    Time: 0 ms (100%), Space: 9 MB (19.96%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -5,15 +5,16 @@ public:
         for(int i=0;i<s.size();i++){
             if(s[i]=='(' || s[i]=='{' || s[i]=='['){
                 st.push(s[i]);
+                continue;
             }
-            else{
+            //else{
             if(st.empty()) return false;
             if((s[i]==')' && st.top()!='(') || (s[i]=='}' && st.top()!='{') || (s[i]==']' && st.top()!='[')){
                 return false;
             }
             st.pop();
         }
-        }
+       // }
         if(!st.empty()) return false;
         return true;
     }
