eaad07c95de6d344e0dbe5f9edf6e746bfc960cf
JOB K SHAJI <142813709+jobkshaji@users.noreply.github.com>
2026-05-26 11:15:58 +0530

    Time: 3 ms (51.26%), Space: 14.1 MB (73.06%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
-    void backtrack(int idx,vector<int>&candidates,int target , vector<int>&curr,vector<vector<int>>&result){
-        if(target==0){
-            result.push_back(curr);
+    void fun(vector<int>&a,int n,int idx,int target,int sum,vector<int>& diary,vector<vector<int>>&res){
+        if(idx==n){
+            if(target==sum){
+                res.push_back(diary);
+            }
             return;
         }
-        if(idx>=candidates.size() || target < 0) return;
-        curr.push_back(candidates[idx]);
-        backtrack(idx,candidates,target-candidates[idx],curr,result);
-        curr.pop_back();
-        backtrack(idx+1,candidates,target,curr,result);
+        if(a[idx]+sum<=target){
+            diary.push_back(a[idx]);
+            sum+=a[idx];
+            fun(a,n,idx,target,sum,diary,res);
+            diary.pop_back();
+            sum-=a[idx];
+        }
+        fun(a,n,idx+1,target,sum,diary,res);
+        return;
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>curr;
-        vector<vector<int>>result;
-        backtrack(0,candidates,target,curr,result);
-        return result;
+
+    vector<vector<int>> combinationSum(vector<int>& a, int target) {
+        int n=a.size();
+        int idx=0;
+        int sum=0;
+        vector<int>diary;
+        vector<vector<int>>res;
+        fun(a,n,idx,target,sum,diary,res);
+        return res;
     }
 };
