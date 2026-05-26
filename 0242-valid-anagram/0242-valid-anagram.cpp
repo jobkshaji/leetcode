4c21025d964691fd2df2f41159ca40de910ceabb
JOB K SHAJI <142813709+jobkshaji@users.noreply.github.com>
2026-05-26 16:55:53 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.49%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int>freq(26,0);
+        if(s.size()!=t.size()){
+            return false;
+        }
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(int i:freq){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
