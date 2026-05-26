1a2cb066b11c25bb749c430d086c2cbfbb640057
JOB K SHAJI <142813709+jobkshaji@users.noreply.github.com>
2026-05-26 10:35:44 +0530

    Time: 3 ms (5.35%), Space: 10.7 MB (5.48%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void fun(string digits,int n,int idx,string diary,vector<string>&res){
+        unordered_map<char,string>f;
+        f['2']="abc";
+        f['3']="def";
+        f['4']="ghi";
+        f['5']="jkl";
+        f['6']="mno";
+        f['7']="pqrs";
+        f['8']="tuv";
+        f['9']="wxyz";
+        if(idx==n){
+            res.push_back(diary);
+            return;
+        }
+        string choice=f[digits[idx]];
+        for(int j=0;j<choice.size();j++){
+            diary.push_back(choice[j]);
+            fun(digits,n,idx+1,diary,res);
+            diary.pop_back();
+        }
+        return;
+    }
+
+    vector<string> letterCombinations(string digits) {
+        unordered_map<char,string>f;
+        f['2']="abc";
+        f['3']="def";
+        f['4']="ghi";
+        f['5']="jkl";
+        f['6']="mno";
+        f['7']="pqrs";
+        f['8']="tuv";
+        f['9']="wxyz";
+        int n=digits.size();
+        int idx=0;
+        string diary;
+        vector<string>res;
+        fun(digits,n,idx,diary,res);
+        return res;
+    }
+};
