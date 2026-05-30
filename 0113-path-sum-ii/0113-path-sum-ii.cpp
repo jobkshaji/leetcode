89b032fb31db724bf81ca22eef95f2c600a95e61
JOB K SHAJI <142813709+jobkshaji@users.noreply.github.com>
2026-05-30 08:43:36 +0530

    Time: 0 ms (100%), Space: 20.9 MB (88.21%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>>res;
+    void fun(TreeNode * root, int sum, int targetSum, vector<int>&diary){
+        if(root==nullptr) return;
+        sum+=root->val;
+        diary.push_back(root->val);
+        if(root->left ==nullptr && root->right==nullptr){
+            if(sum==targetSum){
+                res.push_back(diary);
+                diary.pop_back();
+                return;
+            }
+        }
+        fun(root->left,sum,targetSum,diary);
+        fun(root->right,sum,targetSum,diary);
+        diary.pop_back();
+        return;
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<int>diary;
+        int sum=0;
+        fun(root,sum,targetSum,diary);
+        return res;
+    }
+};
