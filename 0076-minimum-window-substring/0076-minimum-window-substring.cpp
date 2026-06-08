60eb899411ab49d81affb051d6396bc147fb87a1
JOB K SHAJI <142813709+jobkshaji@users.noreply.github.com>
2026-06-08 16:40:19 +0530

    Time: 179 ms (5.11%), Space: 12 MB (7.57%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    bool fine(vector<int>&a,vector<int>&c){
+    bool equal(vector<int>&a,vector<int>&c){
         for(int i=0;i<256;i++){
-            if(a[i]<c[i]){
-                return false;
-            }
+            if(a[i]<c[i]) return false;
         }
         return true;
     }
     string minWindow(string s, string t) {
-        int n=t.size();
+        int l=0;
+        int h=0;
+        int res=INT_MAX;
         int k=s.size();
+        int j=t.size();
+        int start=-1;
         vector<int>a(256,0);
         vector<int>c(256,0);
-        int res=INT_MAX;
-        int l=0,h=0;
-        int start=-1;
-        if(k<n)
-        return "";
-        for(int i=0;i<n;i++){
+        if(j>k) return "";
+        for(int i=0;i<j;i++){
             c[t[i]]++;
         }
         for(h=0;h<k;h++){
             a[s[h]]++;
-            while(fine(a,c)){
+            while(equal(a,c)){
                 int len=h-l+1;
                 if(res>len){
                     res=len;
@@ -33,7 +31,7 @@ public:
                 l++;
             }
         }
-        if(res==INT_MAX) return"";
+        if(res==INT_MAX) return "";
         return s.substr(start,res);
     }
 };
