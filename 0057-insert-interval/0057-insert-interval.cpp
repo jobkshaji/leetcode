9759901c84d0f6f98552eba60550ae7bfcac3484
JOB K SHAJI <142813709+jobkshaji@users.noreply.github.com>
2026-06-15 10:40:17 +0530

    Time: 7 ms (7.12%), Space: 23.1 MB (5.32%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>>merge(vector<vector<int>>& intervals){
+    vector<vector<int>>merge(vector<vector<int>>& ans){
+        sort(ans.begin(),ans.end());
         vector<vector<int>>res;
-        int start1=intervals[0][0];
-        int end1=intervals[0][1];
-        int n=intervals.size();
-        for(int i=1;i<n;i++){
-            int start2=intervals[i][0];
-            int end2=intervals[i][1];
+        int start1=ans[0][0];
+        int end1=ans[0][1];
+        for(int i=1;i<ans.size();i++){
+            int start2=ans[i][0];
+            int end2=ans[i][1];
             if(end1>=start2){
                 start1=start1;
                 end1=max(end1,end2);
@@ -22,21 +22,19 @@ public:
     }
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>>ans;
-        vector<vector<int>>res;
-        int n=intervals.size();
-        bool flag=false;
-        for(int i=0;i<n;i++){
-            int start=intervals[i][0];
-            if(flag == false && start >= newInterval[0] ){
+        int start=newInterval[0];
+        int flag=false;
+        for(int i=0;i<intervals.size();i++){
+            int check=intervals[i][0];
+            if(flag==false && check>=start){
                 ans.push_back({newInterval});
                 flag=true;
             }
             ans.push_back({intervals[i]});
         }
         if(flag==false){
-            ans.push_back({newInterval});
+            ans.push_back(newInterval);
         }
-        res=merge(ans);
-        return res;   
-        }
+        return merge(ans);
+    }
 };
