09a60943043195e8928edfa97f0a3f6e0929c9b6
JOB K SHAJI <142813709+jobkshaji@users.noreply.github.com>
2026-07-09 10:49:55 +0530

    Time: 0 ms (100%), Space: 8.4 MB (10.73%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
+    unordered_map<int,int>dp;
     int fib(int n) {
-        if(n==1) return 1;
-        if(n==0) return 0;
+        if(n==0||n==1){
+            return n;
+        }
+        if(dp.find(n)!=dp.end()){
+            return dp[n];
+        }
         int ans=fib(n-1)+fib(n-2);
+        dp[n]=ans;
         return ans;
     }
 };
