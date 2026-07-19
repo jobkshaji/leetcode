cf5b857768e3ab4269315c5ca8cb110d00ab6423
JOB K SHAJI <142813709+jobkshaji@users.noreply.github.com>
2026-07-19 18:32:45 +0530

    Time: 0 ms (100%), Space: 8.6 MB (94.04%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int>freq(26);
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']=i;
+        }
+        vector<bool>visit(26,false);
+        string st;
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            if(visit[ch-'a']) continue;
+            while(!st.empty()&& st.back()>ch && freq[st.back()-'a']>i){
+                visit[st.back()-'a']=false;
+                st.pop_back();
+            }
+            st.push_back(ch);
+            visit[ch-'a']=true;
+        }
+        return st;
+    }
+};
