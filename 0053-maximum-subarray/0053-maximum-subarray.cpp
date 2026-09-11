ede41729372cfb0e8ad5bb57e3210059d4e8bf50
JOB K SHAJI <142813709+jobkshaji@users.noreply.github.com>
2026-09-11 09:14:51 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,7 +4,7 @@ public:
         int ans=nums[0];
         int best=nums[0];
         for(int i=1;i<nums.size();i++){
-            int v1=best+nums[i];
+            int v1=nums[i]+best;
             int v2=nums[i];
             best=max(v1,v2);
             ans=max(ans,best);
