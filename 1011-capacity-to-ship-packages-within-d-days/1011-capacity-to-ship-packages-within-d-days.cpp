eb9dd8f3c4480ee16955926f0cd35ac5fad40f14
JOB K SHAJI <142813709+jobkshaji@users.noreply.github.com>
2026-09-23 11:04:00 +0530

    Time: 12 ms (50.52%), Space: 35 MB (54.02%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
+    bool fun(vector<int>&weights,int m,int days){
+        int sum=0;
+        int d=1;
+        for(int i=0;i<weights.size();i++){
+            if(sum+weights[i]<=m){
+                sum+=weights[i];
+            }else{
+                d++;
+                sum=weights[i];
+            }
+        }
+        if(d<=days) return true;
+        return false;
+    }
     int shipWithinDays(vector<int>& weights, int days) {
         int l=*max_element(weights.begin(),weights.end());
-        int h=0;
         int n=weights.size();
-        for(int i=0;i<weights.size();i++){
+        int h=0;
+        for(int i=0;i<n;i++){
             h+=weights[i];
         }
         int res=0;
         while(l<=h){
-            int mid=l+(h-l)/2;
-            int sum=0;
-            int d=1;
-            for(int i=0;i<n;i++){
-                if(sum+weights[i]<=mid){
-                    sum+=weights[i];
-                }else{
-                    d++;
-                    sum=weights[i];
-                }
-            }
-            if(d<=days){
-                res=mid;
-                h=mid-1;
+            int m=(l+h)/2;
+            if(fun(weights,m,days)){
+                res=m;
+                h=m-1;
             }else{
-                l=mid+1;
+                l=m+1;
             }
         }
         return res;
